7ccf229faca79868ee4f7f785bf61a14149d6f93
gio-tev <geotevdorashvili@gmail.com>
2022-06-19 16:06:29 +0400

    Firebase storage add for books to load initially

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,37 @@
+import { useState, useEffect } from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
-import DATA from '../../api/book.json';
 import HomeItem from '../components/HomeItem';
 
 const Home = ({ navigation }) => {
+  const [booksData, setBooksData] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await fetch(
+        'https://book-store-ac9bf-default-rtdb.firebaseio.com/books.json'
+      );
+
+      const data = await response.json();
+
+      const transformedBooks = [];
+      for (const [key, value] of Object.entries(data)) {
+        const transforemdBook = {
+          ...value,
+          id: key,
+        };
+        transformedBooks.push(transforemdBook);
+      }
+
+      setBooksData(transformedBooks);
+    };
+    fetchBooks();
+  }, []);
+
   return (
     <View style={styles.booksContainer}>
       <FlatList
         numColumns={2}
-        data={DATA}
+        data={booksData}
         renderItem={({ item }) => (
           <HomeItem item={item} navigation={navigation} />
         )}
